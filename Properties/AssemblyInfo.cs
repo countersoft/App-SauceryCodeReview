547dacbc76487aae63881d079b8e70edf35512ef
gurindergulati <gurinder.gulati@countersoft.com>
2014-04-14 16:54:47 +0100

    Changed project name for consistency

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Saucery")]
+[assembly: AssemblyTitle("SauceryCodeReview")]
 [assembly: AssemblyDescription("Countersoft Saucery")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Countersoft Limited")]
